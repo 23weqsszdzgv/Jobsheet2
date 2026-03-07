4c4d186b65efeab6455f14671e07077727ab74ee
23weqsszdzgv <tegr12q@gmail.com>
2026-03-08 01:14:44 +0700

    dosen class

---
+++ Dosen.java
@@ -0,0 +1,13 @@
+public class Dosen {
+    public String Kode;
+    public String Nama;
+    public Boolean JenisKelamin;
+    public int usia;
+
+    public Dosen(String Kode, String Nama , Boolean JenisKelamin, int usia) {
+        this.Kode = Kode;
+        this.Nama = Nama;
+        this.JenisKelamin = JenisKelamin;
+        this.usia = usia;
+    }
+}
